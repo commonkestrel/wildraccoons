44bd7bc6207204b94a425124c5cfa3bff7316d45
commonkestrel <jettb6@gmail.com>
2024-01-20 12:24:58 -0800

    add static file hosting

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn main() -> tide::Result<()> {
     let mut tera = Tera::new("templates/**/*")?;
     tera.autoescape_on(vec!["html"]);
 
-    tera.render("index.tera", &Context::new()).unwrap();
+    tera.render("index.html", &Context::new()).unwrap();
 
     let mut app = tide::with_state(tera);
     app.with(driftwood::DevLogger);
@@ -20,8 +20,18 @@ async fn main() -> tide::Result<()> {
 
     app.at("/").get(|req: Request<Tera>| async move {
         let tera = req.state();
-        tera.render_response("index.tera", &context! {})
+        tera.render_response("index.html", &context! {}).map(|mut res| {
+            res.set_content_type(tide::http::mime::HTML);
+            res
+        })
     });
+
+    app.at("/static").serve_dir("static")?;
+    app.at("/css").serve_dir("css")?;
+    app.at("/js").serve_dir("js")?;
+    app.at("/robots.txt").serve_file("static/robots.txt")?;
+    app.at("sitemap.txt").serve_file("static/sitemap.txt")?;
+    app.at("favicon.ico").serve_file("static/favicon.ico")?;
 
     app.listen("127.0.0.1:8080").await?;
 
